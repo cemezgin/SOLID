392134a331080cc822cf6569f54d4fb4b2d14519
Eren AKPINAR <eren.akpinar@kolayik.com>
2019-04-17 13:29:15 +0300

    Add interface segregation example

---
+++ InterfaceSegregation/2.cs
@@ -0,0 +1,54 @@
+public interface ILog
+{
+    void Log(string message);
+}
+ 
+public interface IDBLog: ILog
+{
+    void OpenConnection();
+ 
+    void CloseConnection();
+}
+ 
+public interface IFileLog: ILog
+{
+    void CheckFileSize();
+ 
+    void GenerateFileName();
+}
+
+public class FileLogger : IFileLog
+   {
+       public void CheckFileSize()
+       {
+           //Code to check log file size
+       }
+ 
+       public void GenerateFileName()
+       {
+           //Code to generate a new file name
+       }
+        
+       public void Log(string message)
+       {
+           //Code to log data to the log file
+       }
+   }
+ 
+   public class DBLogger : IDBLog
+   {
+       public void Log(string message)
+       {
+           //Code to log data to the database
+       }
+ 
+       public void OpenConnection()
+       {
+           //Code to open database connection
+       }
+ 
+        public void CloseConnection()
+        {
+           //Code to close database connection
+        }
+   }
